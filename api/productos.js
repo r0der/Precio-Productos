1319a189ac67141b40685e4eb38ad5f0c99392e4
r0der <calvorodrigoo@gmail.com>
2026-02-11 17:02:37 -0300

    Update productos.js

--- api/productos.js
+++ api/productos.js
@@ -4,16 +4,25 @@ export default async function handler(req, res) {
     const DATABASE_ID = process.env.DATABASE_ID;
 
     const response = await fetch(
-        `https://api.notion.com/v1/databases/${DATABASE_ID}/query`,
-        {
-            method: "POST",
-            headers: {
-                "Authorization": `Bearer ${NOTION_TOKEN}`,
-                "Notion-Version": "2022-06-28",
-                "Content-Type": "application/json"
-            }
-        }
-    );
+    `https://api.notion.com/v1/databases/${DATABASE_ID}/query`,
+    {
+        method: "POST",
+        headers: {
+            "Authorization": `Bearer ${NOTION_TOKEN}`,
+            "Notion-Version": "2022-06-28",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            sorts: [
+                {
+                    property: "Nombre",
+                    direction: "ascending"
+                }
+            ]
+        })
+    }
+);
+
 
     const data = await response.json();
 
@@ -25,3 +34,4 @@ export default async function handler(req, res) {
 
     res.status(200).json(productos);
 }
+
